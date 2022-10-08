f1d565453228310d1b1150aa43bbd64a9a15a87f
Irina <anatskayairina@gmail.com>
2022-10-08 02:54:17 +0200

    Example005,006 added

---
+++ Example001_HelloConsole/Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("write your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "irina")
+{
+    Console.WriteLine("CIao, TESORO!");
+}
+else
+{
+    Console.Write("Ciao ");
+    Console.WriteLine(username);
+}
